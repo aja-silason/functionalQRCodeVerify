2d0398d311f121790a6e51be0b2bc97091e207ca
Anania Augusto <anania.augusto@baxtterangola.com>
2024-08-11 22:03:41 +0100

    wwip: add route for intermediant a enum

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,20 +1,117 @@
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera"
-import { useState } from "react"
-import { Alert, Button, StyleSheet, Text, View } from "react-native"
+import { useEffect, useState } from "react"
+import { Alert, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 
+
+enum STATE {
+    "motorist", 
+    "vehicle",
+    "auth"
+}
 
 
 
 export const Cam = () => {
     const [permission, requestPermission] = useCameraPermissions();
 
-    const handleBarcodeScanner = ({data}: any) => {
-        console.log(data);
-        if(data == 220560){
-            return Alert.alert("Aviso", "Motorista autorizado");
-        }
-        Alert.alert("Aviso", "Motorista não autorizado");
+    const [scan, setScan] = useState<string | null>(null);
+    const [motorist, setMotorist] = useState<string>()
+    const [vehicle, setVehicle] = useState<string>()
+
+    const [flag, setFlag] = useState<string | STATE>(STATE.motorist)
         
+    const handleBarcodeScannerMotorist = async ({data}: any) => {
+        try {
+            if(!scan){
+                setScan(data);
+                console.log(data);
+    
+                if(data === "220560"){
+                        return Alert.alert("Aviso", "Motorista autorizado",
+                            [{
+                            text: "Transporte",
+                            onPress: () => getMotorist(data)
+                        }]
+                    );
+                }
+                Alert.alert("Aviso", `Motorista não autorizado`, [
+                    {
+                        text: "Tentar novamente",
+                        onPress: () => {}
+                    },
+                    {
+                        text: "Manualmente",
+                        onPress: () => {}
+                    }
+                ]);   
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleBarcodeScannerVehicle = async ({data}: any) => {
+        try {
+            if(!scan){
+                setScan(data);
+                console.log(data);
+    
+                if(data === "220560"){
+                    return Alert.alert("Aviso", "Veiculo autorizado",
+                        [
+                            {
+                                text: "Gerar Autorização",
+                                onPress: () => getVehicle(data)
+                            }
+                        ]
+                    );
+                }
+                Alert.alert("Aviso", `Veiculo não autorizado`, [
+                    {
+                        text: "Tentar novamente",
+                        onPress: () => tryAgain()
+                    },
+                    {
+                        text: "Manualmente",
+                        onPress: () => {}
+                    }
+                ]);   
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const tryAgain = () => {
+        setScan(null);
+    }
+
+    const getMotorist = (data: any) => {
+        setMotorist(data);
+        setScan(null);
+        setFlag(STATE.vehicle)
+        console.log("dados do motorista ", motorist)
+    }
+
+    const getVehicle = (data: any) => {
+        setVehicle(data);
+        setScan(null);
+        setFlag(STATE.auth)
+        console.log("dados do veiculo ", vehicle)
+    }
+
+    const handleSubmitDataAuth = async () => {
+        try {
+            const payload = {
+                motorist: motorist,
+                vehicle: vehicle,
+                time: new Date()
+            }
+            console.log("Payload", payload);
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 
     if(!permission){
@@ -32,12 +129,30 @@ export const Cam = () => {
     
     return (
         <View style={style.constainer}>
-            <CameraView facing="front" style={style.cam} barcodeScannerSettings={{barcodeTypes: ["qr"]}} onBarcodeScanned={handleBarcodeScanner}>
-                <View>
-                    <Text>See this thing</Text>
-                </View>
-            </CameraView>
-            <Text style={style.message}>See this thing</Text>
+            {
+                flag === STATE.motorist ? (
+                    <CameraView facing="back" style={style.cam} barcodeScannerSettings={{barcodeTypes: ["qr"]}} onBarcodeScanned={handleBarcodeScannerMotorist}>
+                        <View>
+                            <Text>See this thing - Motorista</Text>
+                        </View>
+                    </CameraView>
+                ) : flag === STATE.vehicle ? (
+                    <CameraView facing="back" style={style.cam} barcodeScannerSettings={{barcodeTypes: ["qr"]}} onBarcodeScanned={handleBarcodeScannerVehicle}>
+                        <View>
+                            <Text>See this thing - Veiculo</Text>
+                        </View>
+                    </CameraView>
+                ) : (
+                    <View style={style.viewAuth}>
+                        <Text style={style.text}>Motorista: {motorist}</Text>
+                        <Text style={style.text}>Veiculo: {vehicle}</Text>
+                        <TouchableOpacity  onPress={handleSubmitDataAuth} >
+                            <Text>Submeter conteudo</Text>
+                        </TouchableOpacity>
+                        <TextInput style={style.input} keyboardType="decimal-pad"/>
+                    </View>
+                )
+            }
         </View>
     )
 }
@@ -60,44 +175,20 @@ const style = StyleSheet.create({
     message: {
         textAlign: "center",
         backgroundColor: "#ccc",
+    },
+    viewAuth: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center"
+    },
+    text: {
+        color: "#000"
+    },
+    input: {
+        borderWidth: 1,
+        width: '70%',
+        padding: 8,
+        fontSize: 14
     }
 })
-
-
-
-// import React, { useState, useRef } from "react";
-// import { Linking, Text, TouchableOpacity, View } from "react-native";
-// import { RNCamera } from "react-native-camera";
-// import QRCodeScanner from "react-native-qrcode-scanner";
-
-// const App = () => {
-//   const QRCodeRef = useRef<QRCodeScanner | any>(null);
-//   const [link, setLink] = useState<string | any>("");
-
-//   const handleLink = () => {
-//     Linking.openURL(link).catch(() => {
-//       console.log("Ouve um erro");
-//     })
-
-//     QRCodeRef.current!.reactivate();
-//   }
-//   return(
-//     <QRCodeScanner
-//       ref={QRCodeRef}
-//       onRead={({data}) => setLink(data)}
-//       flashMode={RNCamera.Constants.FlashMode.off}
-//       topContent={
-//         <View>
-//           <Text>Conteudo Scanneado{link}</Text>
-//         </View>
-//       }
-//       bottomContent={
-//         <View>
-//           <TouchableOpacity style={{padding: 12, backgroundColor: "#ccc"}} onPress={handleLink}>
-//             <Text style={{color: "#fff"}}>Ir</Text>
-//           </TouchableOpacity>
-//         </View>
-//       }
-//     />
-//   )
-// }
